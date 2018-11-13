aceae876c4b1afb55ff1f2f1c539745ffc84a859
Miguel <malejo@occ.com.mx>
2018-11-13 17:48:28 -0600

    componentes funcionales

--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,10 +1,24 @@
-import React, {Component} from 'react';
+import React from 'react';
 import Media from './media.js';
 import './playlist.css';
+import Play from '../../icons/components/play';
+// class Playlist extends Component {
+//     render(){
+//         const playlist = this.props.data.categories[0].playlist;
+//         return(
+//             <div className="Playlist">
+//             {
+//                 playlist.map((item) =>{
+//                     return <Media {...item} key={item.id} />
+//                 })
+//             }
+//             </div>
+//         )
+//     }
+// }
 
-class Playlist extends Component {
-    render(){
-        const playlist = this.props.data.categories[0].playlist;
+function Playlist(props){
+        const playlist = props.data.categories[0].playlist;
         return(
             <div className="Playlist">
             {
@@ -14,7 +28,6 @@ class Playlist extends Component {
             }
             </div>
         )
-    }
 }
 
 export default Playlist;

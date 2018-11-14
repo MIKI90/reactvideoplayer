1da8eb1095b179f2391661190ccfc04ebdde7685
Miguel <malejo@occ.com.mx>
2018-11-14 13:06:40 -0600

    Se agrgaron componentes my estructura al proyecto

--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,28 +1,12 @@
 import React from 'react';
 import Media from './media.js';
 import './playlist.css';
-import Play from '../../icons/components/play';
-// class Playlist extends Component {
-//     render(){
-//         const playlist = this.props.data.categories[0].playlist;
-//         return(
-//             <div className="Playlist">
-//             {
-//                 playlist.map((item) =>{
-//                     return <Media {...item} key={item.id} />
-//                 })
-//             }
-//             </div>
-//         )
-//     }
-// }
 
 function Playlist(props){
-        const playlist = props.data.categories[0].playlist;
         return(
             <div className="Playlist">
             {
-                playlist.map((item) =>{
+                props.playlist.map((item) =>{
                     return <Media {...item} key={item.id} />
                 })
             }

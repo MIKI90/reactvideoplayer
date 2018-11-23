1a5c96d1932e28848ab7ea270d95d64bf7e7d022
Miguel <malejo@occ.com.mx>
2018-11-23 13:52:14 -0600

    added a container

--- src/pages/containers/home.js
+++ src/pages/containers/home.js
@@ -2,12 +2,16 @@ import React,{Component} from 'react';
 import HomeLayout from '../components/home-layout'
 import Categories from '../../categories/components/categories';
 import Related from '../components/related';
+import ModalContainer from '../../widgets/containers/modal-container';
 class Home extends Component{
     render(){
         return(
             <HomeLayout> 
                <Related/>
                <Categories categories={this.props.data.categories}/> 
+               <ModalContainer>
+                   <p>hola</p>
+               </ModalContainer>
             </HomeLayout>
         )
     }
